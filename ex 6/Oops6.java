5b0a2525a692b94dd82f99764eded4a0f9397020
nishaah16 <69071844+nishaah16@users.noreply.github.com>
2020-09-01 15:07:38 +0530

    Update Oops6.java

--- ex 6/Oops6.java
+++ ex 6/Oops6.java
@@ -3,19 +3,80 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package oops.pkg6;
+package oops6;
 
 /**
  *
- * @author Preetha
+ * @author nisha
  */
-public class Oops6 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+class Stack{
+    static final int MAX=4;
+    int top;
+    int a[]=new int[MAX];
+    boolean isEmpty()
+    {
+        return(top<0);
     }
-    
+    Stack()
+    {
+        top=-1;
+    }
+    boolean push(int x)
+    {
+        if(top>=(MAX-1))
+        {
+            System.out.println("Stack Overflow");
+            return false;
+        }
+        else
+        {
+            a[++top]=x;
+            System.out.println(x+"pushed into stack");
+            return true;
+        }
+    }
+    int pop()
+    {
+        if(top<0)
+        {
+            System.out.println("Stack empty");
+            return 0;
+        }
+        else
+        {
+            int x=a[top--];
+            return x;
+        }   
+    }
+    int peek()
+    {
+        if(top<0)
+        {
+            System.out.println("Stack empty");
+            return 0;
+        }
+        else
+        {
+            int x=a[top];
+            return x;
+        }
+    }
 }
+class Oops6{
+    public static void main(String args[])
+    {
+        Stack s=new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+        System.out.println(s.pop()+" popped from stack");
+        System.out.println(s.pop()+" popped from stack");
+        System.out.println(s.pop()+" popped from stack");
+        System.out.println(s.pop()+" popped from stack");
+        System.out.println(s.pop());
+    }
+}
+
