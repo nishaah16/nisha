b14b77dc186521d4c2bac99f49df16812bc0dfc0
nishaah16 <69071844+nishaah16@users.noreply.github.com>
2020-09-13 20:21:11 +0530

    Update Oopslabex8ii.java

--- oopslab ex 8/Oopslabex8ii.java
+++ oopslab ex 8/Oopslabex8ii.java
@@ -3,19 +3,48 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package oopslabexii;
-
+package oopslabex8ii;
+import java.util.*;
+import static oopslabex8ii.roigi.a;
 /**
  *
- * @author Preetha
+ * @author nisha
  */
-public class Oopslabexii {
+public class Oopslabex8ii {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        String str="ABC";
+        System.out.printf("All permutations with"+" repetition of %s are:\n",str);
+        a(str);
         // TODO code application logic here
-    }
-    
+    }    
 }
+class roigi
+{
+static void b(String str,char[]data,int last,int index)
+{
+    int length=str.length();
+    for(int i=0;i<length;i++)
+    {
+        data[index]=str.charAt(i);
+        if(index==last)
+            System.out.println(new String(data));
+        else
+            b(str,data,last,index+1);
+    }
+}
+public static void a(String str)
+{
+    int length=str.length();
+    char[] data=new char[length+1];
+    char[] temp=str.toCharArray();
+    Arrays.sort(temp);
+    str=new String(temp);
+    b(str,data,length-1,0);
+}
+}
+© 2020 GitHub, Inc.
